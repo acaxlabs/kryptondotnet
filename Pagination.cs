58662d2730b0ebe442c29b121ce991a1c2a09863
Josh Hickerson <josh.hickerson@acaexpress.local>
2017-02-27 13:49:38 -0600

    removed unneeded paginationattribute class added comments

--- Pagination.cs
+++ Pagination.cs
@@ -19,11 +19,21 @@ namespace KryptonDotNet
 {
     public static class PaginationApiController
     {
+        /// <summary>
+        /// Creates a KryptonDotNet.PaginatedResult
+        /// </summary>
+        /// <param name="controller">the current Web Api controller</param>
+        /// <param name="items">the list of objects to page</param>
+        /// <returns>KryptonDotNet.PaginatedResult</returns>
         public static PaginatedResult Paginate(this ApiController controller, IQueryable<object> items)
         {
             return new PaginatedResult(items, controller.ActionContext);
         }
     }
+    /// <summary>
+    ///  Represents a paged list of items as content and paging info in header values
+    ///  : -h krypton-page, -h krypton-total, -h krypton-pageSize
+    /// </summary>
     public class PaginatedResult : ResponseMessageResult
     {
         public PaginatedResult(IQueryable<object> items, HttpActionContext actionContext)
@@ -37,65 +47,24 @@ namespace KryptonDotNet
             pageInfo.Total = pageInfo.Total == 0 ? items.Count() : pageInfo.Total;
             pageInfo.Items = items.Skip((pageInfo.Page - 1) * pageInfo.PageSize).Take(pageInfo.PageSize).ToList();
             this.Response.Content = new ObjectContent(pageInfo.Items.GetType(), pageInfo.Items, actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+
             this.Response.Headers.Remove("krypton-page");
-
             this.Response.Headers.Add("krypton-page", pageInfo.Page.ToString());
+
             this.Response.Headers.Remove("krypton-total");
-
             this.Response.Headers.Add("krypton-total", pageInfo.Total.ToString());
+
             this.Response.Headers.Remove("krypton-pageSize");
-
             this.Response.Headers.Add("krypton-pageSize", pageInfo.PageSize.ToString());
-
         }
-
 
     }
-
-    public class PaginationAttribute : ActionFilterAttribute
-    {
-        PageInfo PageInfo { get; set; }
-
-
-        public override void OnActionExecuting(HttpActionContext actionContext)
-        {
-            base.OnActionExecuting(actionContext);
-            var query = actionContext.Request.RequestUri.ParseQueryString();
-            PageInfo pageInfo = new PageInfo();
-            pageInfo.Page.TryParseExtended(query["page"], pageInfo.Page);
-            pageInfo.Total.TryParseExtended(query["total"], pageInfo.Total);
-            pageInfo.PageSize.TryParseExtended(query["pageSize"], pageInfo.PageSize);
-            this.PageInfo = pageInfo;
-        }
-        public override async void OnActionExecuted(HttpActionExecutedContext actionExecutedContext)
-        {
-            base.OnActionExecuted(actionExecutedContext);
-            var formatter = actionExecutedContext.ActionContext.ControllerContext.Configuration.Formatters.JsonFormatter;
-            //var content = await actionExecutedContext.Response.Content.ReadAsStringAsync();
-            var contentStream = await actionExecutedContext.Response.Content.ReadAsStreamAsync();
-            var items = (IList<object>)new JsonSerializer().Deserialize(new StreamReader(contentStream), typeof(IList<object>));
-            this.PageInfo.Total = this.PageInfo.Total == 0 ? items.Count() : this.PageInfo.Total;
-            this.PageInfo.Items = items.Skip((this.PageInfo.Page - 1) * this.PageInfo.PageSize).Take(this.PageInfo.PageSize).ToList();
-            actionExecutedContext.Response.Content = new ObjectContent(this.PageInfo.GetType(), this.PageInfo, formatter);
-        }
-
-
-    }
-
-
+    
     public class PageInfo
     {
         public int Total = 0;
         public int Page = 1;
         public int PageSize = 100;
         public List<object> Items { get; set; }
-
-        public PageInfo() { }
-        public PageInfo(int page, int total, int pageSize)
-        {
-            Page = page == 0 ? 1 : page;
-            Total = total;
-            PageSize = pageSize == 0 ? 100 : pageSize;
-        }
     }
 }
