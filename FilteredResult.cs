4bf2925242d8f0ac80e8d8f15746f3845a20e635
strunkj <joshua.strunk@getinsured.com>
2018-04-16 15:49:08 -0500

    Add support for filtering by date by using postfixes.

--- FilteredResult.cs
+++ FilteredResult.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http.Controllers;
 using System.Web.Http.Results;
+using Newtonsoft.Json.Linq;
 
 namespace KryptonDotNet
 {
@@ -23,6 +24,16 @@ namespace KryptonDotNet
             : base(new HttpResponseMessage(System.Net.HttpStatusCode.OK))
         {
             var filters = HeaderUtil.ResolveFilterInfoHeader(actionContext.Request.Headers);
+            Items = FilteredResultHelpers.ProcessFilters<T>(items, filters);
+            this.Response.Content = new ObjectContent(Items.GetType(), Items, actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+        }
+
+    }
+
+    public static class FilteredResultHelpers
+    {
+        public static IQueryable<T> ProcessFilters<T>(IQueryable<T> items, JObject filters)
+        {
             if (filters != null)
             {
                 StringBuilder filterClause = new StringBuilder(string.Empty);
@@ -35,21 +46,95 @@ namespace KryptonDotNet
 
                     if (!string.IsNullOrEmpty(filterClause.ToString()))
                         filterClause.Append(" and ");
-                    Type type = typeof(T);
-                    PropertyInfo propertyInfo = type.GetProperty(item.Name, BindingFlags.IgnoreCase| BindingFlags.Public | BindingFlags.Instance);
-                    if (propertyInfo is null) continue;
-                    filterClause.Append(BuildFilterClause(item.Path, item.Value.ToString(), propertyInfo));
+
+                    filterClause.Append(ProcessPropertyIntoFilterClause<T>(item.Name, item.Value));
                 }
-                items = string.IsNullOrEmpty(filterClause.ToString()) ? items : items.Where(filterClause.ToString());
+
+                return string.IsNullOrEmpty(filterClause.ToString()) ? items : items.Where(filterClause.ToString());
             }
-            Items = items;
-            this.Response.Content = new ObjectContent(Items.GetType(), Items, actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+            else
+            {
+                return items;
+            }
         }
 
-        private static string BuildFilterClause(string key, string value, PropertyInfo propertyInfo)
+
+        private static string ProcessPropertyIntoFilterClause<T>(string propName, JToken propValue)
         {
-            Type propertyType = propertyInfo.PropertyType;
-            return propertyType.IsEnum ? $"{key} = \"{value}\"" : $"{key}.ToString() = \"{value}\"";
+            Type type = typeof(T);
+
+            string postfix = null;
+
+            if (PostFixOp("_before", ref propName)) postfix = "_before";
+            else if (PostFixOp("_after", ref propName)) postfix = "_after";
+
+
+            PropertyInfo propertyInfo = type.GetProperty(propName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+            if (propertyInfo is null) return "";
+
+            return BuildFilterClause(propName, propValue.ToString(), propertyInfo.PropertyType, postfix);
+        }
+
+        /** This builds out the dyanmic linq query to filter for this property
+         * ex. 
+         * itemPath:        Contacts.status,
+         * value:           Processing,
+         * propertyType:    String
+         */
+        private static string BuildFilterClause(string itemPath, string value, Type propertyType, string postfix)
+        {
+
+            if (propertyType.IsEnum)
+            {
+                return $"{itemPath} = \"{value}\"";
+            }
+            else if (propertyType.FullName == typeof(DateTime).FullName && postfix != null)
+            {
+
+                var comparer = "=";
+                if (postfix == "_before") comparer = "<=";
+                else if (postfix == "_after") comparer = ">=";
+
+                //normalize passed in date value to 12am
+                var date = new DateTime(long.Parse(value)).Date;
+                value = new DateTime(long.Parse(value)).Date.ToShortDateString();
+
+                if (postfix == "_before") date = date.AddDays(1);
+
+                //Date to get the date set to 12am, Ticks becuase we are comparing utc ticks
+                return $"{itemPath} {comparer} DateTime({date.Year}, {date.Month}, {date.Day})";
+            }
+
+            return $"{itemPath}.toString() = \"{value}\"";
+        }
+
+
+        private static bool PostFixOp(string postfix, ref string name)
+        {
+            if (CheckPostfix(name, postfix))
+            {
+                name = ClipPostfix(name, postfix);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        private static bool CheckPostfix(string toCheck, string postfix)
+        {
+            return toCheck.Length > postfix.Length && toCheck.Substring(toCheck.Length - postfix.Length).ToLower() == postfix;
+        }
+        private static string ClipPostfix(string toClip, string postfix)
+        {
+            if (CheckPostfix(toClip, postfix))
+            {
+                return toClip.Substring(0, toClip.Length - postfix.Length);
+            }
+            else
+            {
+                return toClip;
+            }
         }
     }
 }
