25dc8738be88ad21c29091cc40d2d5237ca9c1ea
strunkj <joshua.strunk@getinsured.com>
2018-04-19 12:33:01 -0500

    Cleaned up solution structure to ensure projects are in distinct folders under solution dir, fix testing project so it is under solution.

---
+++ KryptonDotNet/ApiExceptionFilter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Formatting;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http.Filters;
+using XenonExtensions;
+
+namespace KryptonDotNet
+{
+    public class ApiExceptionFilter : ExceptionFilterAttribute
+    {
+        public override void OnException(HttpActionExecutedContext context)
+        {
+            string message =  context.Exception.AllMessages().Replace(Environment.NewLine, " ");
+            HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
+            response.ReasonPhrase = message;
+            response.Content = new ObjectContent(typeof(Error), new Error("error", message, message), new JsonMediaTypeFormatter());
+            context.Response = response; 
+        }
+    }
+}
---
+++ KryptonDotNet/Error.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KryptonDotNet
+{
+    public class Error
+    {
+        public string Message { get; set; }
+        public object Content { get; set; }
+        public string Type { get; set; } = "Error";
+
+        public Error(string type, string msg, object content)
+        {
+            Type = type;
+            Message = msg;
+            Content = content;
+        }
+
+        public Error(Error error)
+        {
+            Type = error.Type;
+            Message = error.Message;
+            Content = error.Content;
+        }
+    }
+}
