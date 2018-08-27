ce307316e8f855020e8fe7767684066310b2adfe
strunkj <joshua.strunk@getinsured.com>
2018-08-27 17:11:37 -0500

    Have ErrorResult check config before returning detailed error messages.

--- KryptonDotNet/ErrorResult.cs
+++ KryptonDotNet/ErrorResult.cs
@@ -10,6 +10,7 @@ using System.Web.Http.Controllers;
 using System.Web.Http.Results;
 using System.Web.Http;
 using XenonExtensions;
+using System.Web.Configuration;
 
 namespace KryptonDotNet
 {
@@ -18,30 +19,72 @@ namespace KryptonDotNet
         public const string DEFAULT_ERROR_MESSAGE = "An error has occurred";
         public ErrorResult(HttpStatusCode statusCode, string type, string message, object content, HttpActionContext actionContext) : base(new HttpResponseMessage(statusCode))
         {
-            message = string.IsNullOrEmpty(message) ? DEFAULT_ERROR_MESSAGE : message;
-            this.Response.ReasonPhrase = DEFAULT_ERROR_MESSAGE;
-            this.Response.Content = new ObjectContent(typeof(Error), new Error(type, message, content), actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+            bool enableDetailedErrorResults = bool.Parse(WebConfigurationManager.AppSettings["EnableDetailedErrorResults"]);
+
+            if(enableDetailedErrorResults)
+            {
+                message = string.IsNullOrEmpty(message) ? DEFAULT_ERROR_MESSAGE : message;
+                this.Response.ReasonPhrase = DEFAULT_ERROR_MESSAGE;
+                this.Response.Content = new ObjectContent(typeof(Error), new Error(type, message, content), actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+            }
+            else
+            {
+                SetupUndetailedResult(actionContext);
+            }
         }
         public ErrorResult(HttpStatusCode statusCode, string type, string message, object content, Exception ex, HttpActionContext actionContext) : base(new HttpResponseMessage(statusCode))
         {
-            message = string.IsNullOrEmpty(message) ? ex.AllMessages().Replace(Environment.NewLine, " ") : message;
-            this.Response.ReasonPhrase = ex.AllMessages().Replace(Environment.NewLine, " ");
-            this.Response.Content = new ObjectContent(typeof(Error), new Error(type, message, content), actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+            bool enableDetailedErrorResults = bool.Parse(WebConfigurationManager.AppSettings["EnableDetailedErrorResults"]);
+            if(enableDetailedErrorResults)
+            {
+                message = string.IsNullOrEmpty(message) ? ex.AllMessages().Replace(Environment.NewLine, " ") : message;
+                this.Response.ReasonPhrase = ex.AllMessages().Replace(Environment.NewLine, " ");
+                this.Response.Content = new ObjectContent(typeof(Error), new Error(type, message, content), actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+            }
+            else
+            {
+                SetupUndetailedResult(actionContext);
+            }
         }
 
         public ErrorResult(HttpStatusCode statusCode, Error error, Exception ex, HttpActionContext actionContext) : base(new HttpResponseMessage(statusCode))
         {
-            error.Message = string.IsNullOrEmpty(error.Message) ? ex.AllMessages().Replace(Environment.NewLine, " ") : error.Message;
-            this.Response.ReasonPhrase = ex?.AllMessages().Replace(Environment.NewLine, " ") ?? "";
-            this.Response.Content = new ObjectContent(typeof(Error), error, actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+            bool enableDetailedErrorResults = bool.Parse(WebConfigurationManager.AppSettings["EnableDetailedErrorResults"]);
+            if(enableDetailedErrorResults)
+            {
+                error.Message = string.IsNullOrEmpty(error.Message) ? ex.AllMessages().Replace(Environment.NewLine, " ") : error.Message;
+                this.Response.ReasonPhrase = ex?.AllMessages().Replace(Environment.NewLine, " ") ?? "";
+                this.Response.Content = new ObjectContent(typeof(Error), error, actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+            }
+            else
+            {
+                SetupUndetailedResult(actionContext);
+            }
         }
 
         public ErrorResult(Exception ex, HttpActionContext actionContext) : base(new HttpResponseMessage(HttpStatusCode.InternalServerError))
         {
-            string message = ex.AllMessages().Replace(Environment.NewLine, " ");
-            this.Response.ReasonPhrase = message;
-            this.Response.Content = new ObjectContent(typeof(Error), new Error("error", message, message), actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+            bool enableDetailedErrorResults = bool.Parse(WebConfigurationManager.AppSettings["EnableDetailedErrorResults"]);
+
+            if (enableDetailedErrorResults)
+            {
+                string message = ex.AllMessages().Replace(Environment.NewLine, " ");
+                this.Response.ReasonPhrase = message;
+                this.Response.Content = new ObjectContent(typeof(Error), new Error("error", message, message), actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+            }
+            else
+            {
+                SetupUndetailedResult(actionContext);
+            }
         }
+
+
+        private void SetupUndetailedResult(HttpActionContext actionContext)
+        {
+            this.Response.ReasonPhrase = DEFAULT_ERROR_MESSAGE;
+            this.Response.Content = new ObjectContent(typeof(Error), new Error("Error", DEFAULT_ERROR_MESSAGE, new { }), actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+        }
+
     }
 
     
