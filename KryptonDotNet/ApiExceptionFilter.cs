7c94bd9c47480b48dcd67a0385929d49e046118e
strunkj <joshua.strunk@getinsured.com>
2018-08-27 17:58:58 -0500

    Add the logic for stripping details from errors to ApiExceptionFilter

--- KryptonDotNet/ApiExceptionFilter.cs
+++ KryptonDotNet/ApiExceptionFilter.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Configuration;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
@@ -14,14 +15,39 @@ namespace KryptonDotNet
 {
     public class ApiExceptionFilter : ExceptionFilterAttribute
     {
+        private string DEFAULT_ERROR_MESSAGE= "";
+
         public override void OnException(HttpActionExecutedContext context)
-        {
-            Exception ex = context.Exception;
-            string message = Regex.Replace(ex.AllMessages(), @"\r\n?|\n", "-");
+        { 
             HttpResponseMessage response = new HttpResponseMessage(HttpStatusCode.InternalServerError);
-            response.ReasonPhrase = message;
-            response.Content = new ObjectContent(typeof(Error), new Error("error", message, ex), new JsonMediaTypeFormatter());
-            context.Response = response; 
+
+            if(CanReturnDetailedErrors())
+            {
+                Exception ex = context.Exception;
+                string message = Regex.Replace(ex.AllMessages(), @"\r\n?|\n", "-");
+                response.ReasonPhrase = message;
+                response.Content = new ObjectContent(typeof(Error), new Error("error", message, ex), new JsonMediaTypeFormatter());
+            }
+            else
+            {
+                response.ReasonPhrase = DEFAULT_ERROR_MESSAGE;
+                response.Content = new ObjectContent(typeof(Error), new Error("Error", DEFAULT_ERROR_MESSAGE, new { }), new JsonMediaTypeFormatter());
+            }
+
+            context.Response = response;
+        }
+
+
+        private bool CanReturnDetailedErrors()
+        {
+            if (ConfigurationManager.AppSettings.AllKeys.Contains("DisableDetailedErrorResults"))
+            {
+                return !bool.Parse(ConfigurationManager.AppSettings["DisableDetailedErrorResults"]);
+            }
+            else
+            {
+                return true;
+            }
         }
     }
 }
