61a93d3416461b0abe263bce8d0a35b9548c1cae
Rama Moravaneni <rama.moravaneni@getinsured.com>
2018-10-23 09:40:38 -0700

    remove new line string of type \n

--- KryptonDotNet/ErrorResult.cs
+++ KryptonDotNet/ErrorResult.cs
@@ -24,23 +24,38 @@ namespace KryptonDotNet
         }
         public ErrorResult(HttpStatusCode statusCode, string type, string message, object content, Exception ex, HttpActionContext actionContext) : base(new HttpResponseMessage(statusCode))
         {
-            message = string.IsNullOrEmpty(message) ? ex.AllMessages().Replace(Environment.NewLine, " ") : message;
-            this.Response.ReasonPhrase = ex.AllMessages().Replace(Environment.NewLine, " ");
+            message = string.IsNullOrEmpty(message) ? GetMessageWithoutNewLines(ex) : message;
+            this.Response.ReasonPhrase = GetMessageWithoutNewLines(ex);
             this.Response.Content = new ObjectContent(typeof(Error), new Error(type, message, content), actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
         }
 
         public ErrorResult(HttpStatusCode statusCode, Error error, Exception ex, HttpActionContext actionContext) : base(new HttpResponseMessage(statusCode))
         {
-            error.Message = string.IsNullOrEmpty(error.Message) ? ex.AllMessages().Replace(Environment.NewLine, " ") : error.Message;
-            this.Response.ReasonPhrase = ex?.AllMessages().Replace(Environment.NewLine, " ") ?? "";
+            error.Message = string.IsNullOrEmpty(error.Message) ? GetMessageWithoutNewLines(ex) : error.Message;
+            this.Response.ReasonPhrase = GetMessageWithoutNewLines(ex);
             this.Response.Content = new ObjectContent(typeof(Error), error, actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
         }
 
         public ErrorResult(Exception ex, HttpActionContext actionContext) : base(new HttpResponseMessage(HttpStatusCode.InternalServerError))
         {
-            string message = ex.AllMessages().Replace(Environment.NewLine, " ");
+            string message = GetMessageWithoutNewLines(ex);
             this.Response.ReasonPhrase = message;
             this.Response.Content = new ObjectContent(typeof(Error), new Error("error", message, message), actionContext.ControllerContext.Configuration.Formatters.JsonFormatter);
+        }
+
+        private string GetMessageWithoutNewLines(Exception ex)
+        {
+            if (ex == null)
+            {
+                return string.Empty;
+            }
+
+            string msg = ex
+                .AllMessages()
+                .Replace(Environment.NewLine, " ")  // \r\n
+                .Replace("\n", " ");
+
+            return msg;
         }
     }
 
